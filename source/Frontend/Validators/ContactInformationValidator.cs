1e10c8eecc37ab9a0df40aa9c1ec14c887be0104
Zuzanna Wójtowicz <zuzannawojtowicz42@gmail.com>
2023-11-14 18:50:22 +0100

    add file structure

---
+++ source/Frontend/Validators/ContactInformationValidator.cs
@@ -0,0 +1,14 @@
+﻿using Frontend.Validators.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frontend.Validators
+{
+    public class ContactInformationValidator : IContactInformationValidator
+    {
+
+    }
+}
