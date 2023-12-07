a85f073767f8b327a2720cebcab57973c0902d85
domser2002 <domser2002@o2.pl>
2023-12-07 14:04:58 +0100

    fix for frontend to controller requests

---
+++ source/Infrastructure/FakeInquiryRepository.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Model;
+
+namespace Api.Infrastructure
+{
+    public class FakeInquiryRepository : IInquireRepository
+    {
+        private IDictionary<int, Inquiry> inquiries;
+        public FakeInquiryRepository()
+        {
+            this.inquiries = new Dictionary<int,Inquiry>();
+        }
+
+        public List<Inquiry> GetAll()
+        {
+            return this.inquiries.Values.ToList();
+        }
+
+        public void AddInquiry(Inquiry inquiry)
+        {
+            inquiries.Add(inquiries.Count + 1, inquiry);
+        }
+    }
+}
