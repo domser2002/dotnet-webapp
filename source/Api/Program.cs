442532da88cf1af499e0ae2bc39e207fb5b8fded
domser2002 <domser2002@o2.pl>
2023-12-07 14:04:58 +0100

    fix for frontend to controller requests

--- source/Api/Program.cs
+++ source/Api/Program.cs
@@ -2,6 +2,7 @@ using Domain.Abstractions;
 using Frontend.Validators;
 using Frontend.Validators.Abstractions;
 using Infrastructure.Repositories;
+using Infrastructure;
 using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
