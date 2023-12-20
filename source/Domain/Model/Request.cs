231daa6c9f9721159251b4029de7768b2d9ec1b3
domser2002 <domser2002@o2.pl>
2023-12-20 14:35:55 +0100

    request class added

---
+++ source/Domain/Model/Request.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model
+{
+    public class Request:Base
+    {
+        public Package Package { get; set; }
+        public Address SourceAddress { get; set; }
+        public Address DestinationAddress { get; set; }
+        public DateTime PickupDate { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public string CompanyName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
