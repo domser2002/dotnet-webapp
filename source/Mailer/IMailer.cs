04ff5621ff093b85c4a7e6851d8109087e2b70be
Zuzanna Wójtowicz <zuzannawojtowicz42@gmail.com>
2024-01-01 19:05:29 +0100

    add IMailer interface

---
+++ source/Mailer/IMailer.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Model;
+
+namespace Mailer;
+
+public interface IMailer
+{
+    public void SendRegistrationMail(User to);
+    public void SendRequestAcceptedMail(Request request, string agreement, string recepit);
+    public void SendPackagePickedUpMail(Request request);
+    public void SendPackageDeveliredMail(Request request);
+    public void SendDeliveryFailedMail(Request request, User courier, string reason);
+}
