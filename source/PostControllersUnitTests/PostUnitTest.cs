3fe4e1de71e477dd33b28addcd61075dbf9aa207
domser2002 <domser2002@o2.pl>
2024-01-16 22:32:30 +0100

    post unit tests

--- source/PostControllersUnitTests/PostUnitTest.cs
+++ source/PostControllersUnitTests/PostUnitTest.cs
@@ -1,11 +1,72 @@
+using Api.Controllers;
+using Domain.Model;
+using Frontend.Validators;
+using Infrastructure.FakeRepositories;
+using Microsoft.AspNetCore.Mvc;
+using ValidatorUnitTests;
+
 namespace PostControllersUnitTests
 {
     [TestClass]
     public class PostUnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PostUsersTest()
         {
+            //Arrange
+            FakeUserRepository repository = new();
+            RegistrationValidator validator = new(1, 10);
+            UsersController controller = new(repository, validator);
+            int count_before = repository.GetAll().Count;
+            //Act
+            User user = UnitTestRegistrationValidator.ValidUser();
+            controller.Create(user);
+            int count_after = repository.GetAll().Count;    
+            //Assert
+            Assert.AreEqual(count_before+1, count_after);
+        }
+        [TestMethod]
+        public void PostOffersTest()
+        {
+            //Arrange
+            FakeOfferRepository repository = new();
+            OffersController controller = new(repository);
+            int count_before = repository.GetAll().Count;
+            //Act
+            Offer offer = new();
+            controller.Create(offer);
+            int count_after = repository.GetAll().Count;
+            //Assert
+            Assert.AreEqual(count_before + 1, count_after);
+        }
+        [TestMethod]
+        public void PostContactInformationTest()
+        {
+            //Arrange
+            FakeContactInformationRepository repository = new();
+            ContactInformationValidator validator = new(1, 10);
+            ContactInformationController controller = new(repository,validator);
+            int count_before = repository.GetAll().Count;
+            //Act
+            ContactInformation contact = ContactInformationValidatorUnitTests.ValidContactInformation();
+            controller.Create(contact);
+            int count_after = repository.GetAll().Count;
+            //Assert
+            Assert.AreEqual(count_before + 1, count_after);
+        }
+        [TestMethod]
+        public void PostRequestsTest()
+        {
+            //Arrange
+            FakeRequestRepository repository = new();
+            RequestController controller = new(repository);
+            int count_before = repository.GetAll().Count;
+            //Act
+            Request request = new();
+            controller.Create(request);
+            int count_after = repository.GetAll().Count;
+            //Assert
+            Assert.AreEqual(count_before + 1, count_after);
         }
     }
 }
