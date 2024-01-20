67521ba117abfda26c3d4b6e4baa784eb8d927dd
domser2002 <domser2002@o2.pl>
2024-01-20 15:18:34 +0100

    create LectureOfferRepository

---
+++ source/Infrastructure/LectureRepositories/LectureOfferRepository.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Abstractions;
+using Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.LectureRepositories
+{
+    public class LectureOfferRepository : IOfferRepository
+    {
+        public void AddOffer(Offer offer)
+        {
+            throw new NotImplementedException();
+        }
+        public void Deactivate(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Offer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Offer GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Offer> GetByInquiry(Inquiry inquiry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
