89028616d0dce75c0fb6908f7e214e625f629ff1
domser2002 <domser2002@o2.pl>
2024-01-20 17:50:14 +0100

    adapters implemented

--- source/Infrastructure/LectureRepositories/LectureOfferRepository.cs
+++ source/Infrastructure/LectureRepositories/LectureOfferRepository.cs
@@ -2,14 +2,35 @@
 using Domain.Model;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Infrastructure.LectureRepositories
 {
     public class LectureOfferRepository : IOfferRepository
     {
+        private const string ApiEndpoint = "https://mini.currier.api.snet.com.pl/Inquires";
+        private const int ApiTimeoutMilliseconds = 5000;
+        private const string ClientId = "your_client_id";
+        private const string SecretId = "your_secret_id";
+
+        private async Task<HttpResponseMessage> MakeHttpPostRequest(string url, string jsonBody)
+        {
+            using (HttpClient client = new())
+            {
+                // Add client_id and secret_id to the request headers
+                client.DefaultRequestHeaders.Add("client_id", ClientId);
+                client.DefaultRequestHeaders.Add("secret_id", SecretId);
+
+                StringContent content = new(jsonBody, Encoding.UTF8, "application/json");
+
+                // Make the HTTP POST request with a timeout
+                return await client.PostAsync(url, content);
+            }
+        }
         public void AddOffer(Offer offer)
         {
             throw new NotImplementedException();
@@ -29,9 +50,51 @@ namespace Infrastructure.LectureRepositories
             throw new NotImplementedException();
         }
 
-        public List<Offer> GetByInquiry(Inquiry inquiry)
+        public async Task<List<Offer>> GetByInquiry(Inquiry inquiry)
         {
-            throw new NotImplementedException();
+            try
+            {
+                // Convert Inquiry object to JSON
+                string jsonBody = JsonConvert.SerializeObject(new { inquiry });
+
+                // Make HTTP POST request with client_id and secret_id headers
+                HttpResponseMessage response = await MakeHttpPostRequest(ApiEndpoint, jsonBody);
+
+                // Check if the request was successful
+                if (response.IsSuccessStatusCode)
+                {
+                    // Parse the response and get the price
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    PriceResponse priceResponse = JsonConvert.DeserializeObject<PriceResponse>(responseBody);
+
+                    if (priceResponse != null && priceResponse.Price.HasValue)
+                    {
+                        // Return the list of offers (assuming PriceResponse has an Offers property)
+                        return priceResponse.Offers;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Failed to parse price from the response.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"HTTP request failed with status code: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+
+            // Return an empty list if there's an error
+            return new List<Offer>();
+        }
+        private class PriceResponse
+        {
+            public decimal? Price { get; set; }
+            public List<Offer> Offers { get; set; }
+            // Add other properties as needed
         }
     }
 }
